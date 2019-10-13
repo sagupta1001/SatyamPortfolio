50b3969863ae8c7af8726f76b5bbaccb2ba5a7bd
Satyam Gupta <sagupta1001@gmail.com>
2019-10-12 20:28:31 -0400

    Implemented edit portfolio functionality (kinda)

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,21 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.new
     end
 
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+          else
+            format.html { render :edit }
+          end
+        end
+    end
+
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -17,5 +32,9 @@ class PortfoliosController < ApplicationController
             format.html { render :new }
         end
     end
+
+
+    private
+
   end
 end
