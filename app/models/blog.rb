0e8bb9c833698da091713c43f9d2100ab4bcd880
Satyam Gupta <sagupta1001@gmail.com>
2019-10-19 17:27:58 -0400

    Adding validations for skills, blogs and portfolios

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,9 @@
 class Blog < ApplicationRecord
-    enum status: {draft: 0, published: 1}
     extend FriendlyId
     friendly_id :title, use: :slugged
+
+    enum status: {draft: 0, published: 1}
+    
+    validates_presence_of :title, :body
+
 end
