e6bd2cfcd878f035780c04bd40863895629378af
Satyam Gupta <38059761+sagupta1001@users.noreply.github.com>
2019-10-19 21:46:57 -0400

    Data feature (#3)
    * Adding validations for skills, blogs and portfolios
    
    * Adding blog topics
    
    * Implemented custom scope for portfolio items
    
    * Added defaults for portfolio image urls
    
    * Adding placeholder concern. Added technology model and skill badge model
    
    * Implemented nested attributes for portfolio items
    
    * Nested attributes for portfolios view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,12 +3,17 @@ class PortfoliosController < ApplicationController
         @portfolio_items = Portfolio.all
     end
 
+    def angular
+        @angular_portfolio_items = Portfolio.angular
+    end
+
     def show
         @portfolio_item = Portfolio.find(params[:id])
     end
 
     def new
         @portfolio_item = Portfolio.new
+        3.times { @portfolio_item.technologies.build }
     end
 
     def edit
@@ -35,17 +40,15 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+            technologies_attributes: [:name]))
 
         respond_to do |format|
-        if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        else
-            format.html { render :new }
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+            else
+                format.html { render :new }
+            end
         end
     end
-
-    private
-
-  end
 end
