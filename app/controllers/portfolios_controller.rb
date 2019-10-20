51796cd6974726e06a9b007856e03cee3bd4a2cc
Satyam Gupta <sagupta1001@gmail.com>
2019-10-19 21:35:57 -0400

    Nested attributes for portfolios view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,6 +13,7 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
+        3.times { @portfolio_item.technologies.build }
     end
 
     def edit
@@ -39,7 +40,8 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+            technologies_attributes: [:name]))
 
         respond_to do |format|
             if @portfolio_item.save
