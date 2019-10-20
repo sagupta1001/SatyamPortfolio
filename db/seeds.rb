5cf72f2d086c1364feedfd53b612bd6261f79152
Satyam Gupta <sagupta1001@gmail.com>
2019-10-19 21:03:39 -0400

    Adding placeholder concern. Added technology model and skill badge model

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,11 @@ end
         thumb_image: "https://place-hold.it/350x200",
     )
 end
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+
+puts "3 technologies created"
