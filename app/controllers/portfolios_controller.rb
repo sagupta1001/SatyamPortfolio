7adc30de9ecb642ae191b442caaf4f025d52e651
Satyam Gupta <sagupta1001@gmail.com>
2019-11-03 21:11:39 -0500

    Refactored portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -23,7 +23,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
           else
             format.html { render :edit }
@@ -40,8 +40,7 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-            technologies_attributes: [:name]))
+        @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
             if @portfolio_item.save
@@ -50,5 +49,14 @@ class PortfoliosController < ApplicationController
                 format.html { render :new }
             end
         end
+    end
+
+    private
+
+    def portfolio_params
+        params.require(:portfolio).permit(:title,
+                                          :subtitle,
+                                          :body,
+                                          technologies_attributes: [:name])
     end
 end
