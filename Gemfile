e347543ef3d9ef18004ec919ef432fe4e10af896
Satyam Gupta <sagupta1001@gmail.com>
2020-03-18 21:01:03 -0400

    Adding jquery ui

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'jquery-rails'
 gem 'satyam_portfolio_view_tool', '~> 0.1.0'
 
 gem 'petergate', '~> 2.0', '>= 2.0.1'
+
+gem 'jquery-ui-rails', '~> 6.0', '>= 6.0.1'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
 //= require jquery3
+//= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
+
+require("packs/html.sortable")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
